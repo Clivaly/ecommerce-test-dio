7fad98bebf2d4de5da013b793bd959cd18296350
Clivaly-Cardoso <clivalydev@gmail.com>
2021-09-03 11:17:25 -0300

    Ecommerce test project Digital Innovation One

---
+++ ecommerce-checkout-api-master/src/main/java/com/clivaly/ecommerce/checkout/config/StreamingConfig.java
@@ -0,0 +1,13 @@
+package com.clivaly.ecommerce.checkout.config;
+
+import com.clivaly.ecommerce.checkout.streaming.CheckoutCreatedSource;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBinding(value = {
+        CheckoutCreatedSource.class,
+//        PaymentPaidSink.class
+})
+public class StreamingConfig {
+}
